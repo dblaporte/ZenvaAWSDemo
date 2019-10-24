4b8587cb3d5434413d879852e0da6063c93397f0
Dianna <b.dianna@ymail.com>
2019-10-24 12:05:48 -0400

    small change

--- index.js
+++ index.js
@@ -19,4 +19,4 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hjs");
 
 app.listen(5000);
-console.log("Server running on http://localhost:5000 ");
+console.log("Server running on http://localhost:5000 hello world");
